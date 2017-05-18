440deb14db5e64cab33e99f9e8b5d26ebcbaa9a3
noar-t <noahthornton15@gmail.com>
2017-05-18 13:33:10 -0500

    Adding singleton

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -0,0 +1,22 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.content.Context;
+
+/**
+ * Created by noah on 5/18/17.
+ */
+
+public class CrimeLab {
+    private static CrimeLab sCrimeLab;
+
+    public static CrimeLab get(Context context) {
+        if (sCrimeLab == null) {
+            sCrimeLab == new CrimeLab(context);
+        }
+        return sCrimeLab;
+    }
+
+    private CrimeLab(Context context) {
+
+    }
+}
