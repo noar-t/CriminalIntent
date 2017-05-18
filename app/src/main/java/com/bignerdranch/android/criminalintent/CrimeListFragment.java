df5ccffea8e2586255143bb4ad1a8da46e5baa8c
noar-t <noahthornton15@gmail.com>
2017-05-18 15:51:01 -0500

    Maifest now goes straight to crimelistactivuty

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -0,0 +1,12 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by noah on 5/18/17.
+ */
+
+public class CrimeListFragment extends Fragment {
+
+    // TODO stuff
+}
