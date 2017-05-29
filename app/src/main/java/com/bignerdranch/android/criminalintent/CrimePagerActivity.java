e72a4966833a8578ac0935e5c37969d4b027f923
noar-t <noahthornton15@gmail.com>
2017-05-28 20:52:14 -0500

    Setting up pagerview logic

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
@@ -1,7 +1,13 @@
 package com.bignerdranch.android.criminalintent;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+
+import java.util.List;
 
 /**
  * Created by noah on 5/28/17.
@@ -9,9 +15,30 @@ import android.support.v7.app.AppCompatActivity;
 
 public class CrimePagerActivity extends AppCompatActivity {
 
+    private ViewPager mViewPager;
+    private List<Crime> mCrimes;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crime_pager);
+        setContentView(R.layout.activity_crime_pager); // get the layout
+
+        mViewPager = (ViewPager) findViewById(R.id.crime_view_pager); // get id for layout file
+
+        mCrimes = CrimeLab.get(this).getCrimes(); // gets the crimes in a list
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
+            @Override
+            public Fragment getItem(int position) {
+                Crime crime = mCrimes.get(position);
+                return CrimeFragment.newInstance(crime.getId());
+            }
+
+            @Override
+            public int getCount() {
+                return mCrimes.size();
+            }
+        });
+
     }
 }
