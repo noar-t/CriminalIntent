6be2d2e42e9b44ffeca9d362d831200572282bc0
noar-t <noahthornton15@gmail.com>
2017-08-07 18:28:13 -0500

    Working on implementing a database for crimes. This commit contains some schema stuff for defining the columns.

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/databse/CrimeDbSchema.java
@@ -0,0 +1,19 @@
+package com.bignerdranch.android.criminalintent.databse;
+
+/**
+ * Created by noah on 8/7/17.
+ */
+
+public class CrimeDbSchema {
+    public static final class CrimeTable {
+        public static final String NAME = "crime";
+
+        public static final class Cols {
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String DATE = "date";
+            public static final String SOLVED = "solved";
+        }
+    }
+
+}
