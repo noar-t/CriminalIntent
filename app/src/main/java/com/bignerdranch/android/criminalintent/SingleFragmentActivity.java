dac7b7fbcb32062bda0fa2cdf937c4152ed9c958
noar-t <noahthornton15@gmail.com>
2017-08-16 16:29:05 -0500

    Setting up layout and reformatting single fragment activity for tablet layout

--- app/src/main/java/com/bignerdranch/android/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/SingleFragmentActivity.java
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.criminalintent;
 
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
@@ -14,10 +15,15 @@ public abstract class SingleFragmentActivity extends AppCompatActivity {
     // boiler plate for single fragment activity layouts
     protected abstract Fragment createFragment();
 
+    @LayoutRes
+    protected int getLayoutResId() {
+        return R.layout.activity_fragment;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
+        setContentView(getLayoutResId());
 
         FragmentManager fm = getSupportFragmentManager(); // retrieve frag manager
         Fragment fragment = fm.findFragmentById(R.id.fragment_container); // allocate a place and id for frag
